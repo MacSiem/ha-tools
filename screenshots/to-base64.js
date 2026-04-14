b8e91668b6e80b16ea5354449fae19b91a68bbfa
Maciej Sieminski <maciek.sieminski@gmail.com>
2026-04-14 12:10:26 +0200

    chore: remove screenshots (user to provide fresh ones)

--- screenshots/to-base64.js
+++
@@ -1,9 +0,0 @@
-// Read a PNG file and output its base64
-// Usage: node to-base64.js <filename>
-const fs = require('fs');
-const path = require('path');
-const file = process.argv[2];
-if (!file) { console.error('Usage: node to-base64.js <file>'); process.exit(1); }
-const fullPath = path.join(__dirname, file);
-const buf = fs.readFileSync(fullPath);
-console.log(buf.toString('base64'));
